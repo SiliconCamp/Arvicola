c1ee52e76bec051db7f766dac3d54459499fe555
SiliconCamp <camp@silicon-camp.ru>
2018-10-18 19:19:33 +0300

    now lines are counted in var and we can move forward of back inside cycle

--- bash_getpage.py
+++ bash_getpage.py
@@ -4,7 +4,7 @@ import time
 bash_url = "http://bash.im/random"
 bash_headers = {"user-agent": "my-app/0.0.1"}
 
-for generate in range(1, 10):
+for generate in range(1, 3):
     bash_filename = str(generate) + "index.html"
     print(bash_filename + ": generating")
     bash_page = requests.get(bash_url, headers=bash_headers, timeout=None)
@@ -12,11 +12,13 @@ for generate in range(1, 10):
     bash_text = bash_text.split("<")
 
     bash_file = open(bash_filename, 'w', encoding='windows-1251')
-    bash_file.write("<")
 
-    for bash_line in bash_text:
-        if bash_line.find('class=\"id\"') > 0:
-            bash_file.write(bash_line + "</a><")
+    line = 0
+    print("Lines =", len(bash_text))
+    while line < len(bash_text):
+        if bash_text[line].find("class=\"text\"") > 0:
+            bash_file.write("<" + bash_text[line])
+        line += 1
 
     bash_file.close()
     time.sleep(10)
