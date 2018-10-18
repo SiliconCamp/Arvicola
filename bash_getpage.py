68119dbc249714a72d356bf00500f62f938b402b
SiliconCamp <camp@silicon-camp.ru>
2018-10-18 18:12:24 +0300

    Added parsing by row content

--- bash_getpage.py
+++ bash_getpage.py
@@ -9,11 +9,14 @@ for generate in range(1, 10):
     print(bash_filename + ": generating")
     bash_page = requests.get(bash_url, headers=bash_headers, timeout=None)
     bash_text = bash_page.text
+    bash_text = bash_text.split("<")
 
     bash_file = open(bash_filename, 'w', encoding='windows-1251')
+    bash_file.write("<")
 
     for bash_line in bash_text:
-            bash_file.write(bash_line)
+        if bash_line.find('class=\"id\"') > 0:
+            bash_file.write(bash_line + "</a><")
 
     bash_file.close()
     time.sleep(10)
