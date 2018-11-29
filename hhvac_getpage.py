50b5bee734e68c6050e5d55d26621abc0fd32393
SiliconCamp <camp@silicon-camp.ru>
2018-11-29 16:30:47 +0300

    Added some comments. Moved file open from the body of cycle (optimization)

--- hhvac_getpage.py
+++ hhvac_getpage.py
@@ -2,6 +2,32 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+'''
+Данный модуль реализует загрузку названий всех компаний, разместивших вакансии на hh.ru по определенному запросу.
+Например, все вакансии около определенных станций метро. Или вакансии в той или иной проф.области.
+
+Используемые модули: requests (загрузка веб-страниц), time (контроль затрачиваемого времени и паузы между запросами),  
+BeautifulSoup (версия 4, разбор содержимого страниц)
+
+Значения переменных:
+hh_url - шаблон поискового GET-запроса, передает параметры и ограничения поиска.
+hh_headers - заголовки HTTP-запроса, которые модуль передает серверу hh.ru
+hh_curl - поисковый GET-запрос с указанием конкретной страницы поиска для разбора
+hh_page - объект, содержащий полученную поисковую страницу
+soup - объект BeautifulSoup, содержащий обработанную структуру поисковой страницы
+last_page - переменная, которая содержит тэг с номером последней из поисковых страниц по запросу для прохода всех.
+hh_filename - имя файла для сохранения результатов
+companies_unique - накопительный список уникальных неповторяющихся названий компаний, чтобы исключить повторы.
+hh_file - объект для обращения к файлу записи результатов.
+page - номер полученной и обрабатываемой поисковой страницы по запросу.
+curtime - время начала работы цикла обработки страницы, нужно для вычисления длительности обработки
+hh_text - исходный текст поисковой страницы, полученной по запросу. 
+
+
+
+
+'''
+
 hh_url = "https://hh.ru/search/vacancy?area=1&clusters=true&enable_snippets=true&items_on_page=100&metro=4.179&" \
          "metro=95.537&metro=97.603&metro=4.172&no_magic=true&order_by=publication_time&page="
 
@@ -16,6 +42,8 @@ print(last_page[-1].text)
 hh_filename = "compparsed.html"
 companies_unique = []
 
+hh_file = open(hh_filename, 'a', encoding='utf-8')
+
 for page in range(0, int(last_page[-1].text)):
 
     curtime = time.time()
@@ -26,8 +54,6 @@ for page in range(0, int(last_page[-1].text)):
     hh_page = requests.get(hh_curl, headers=hh_headers, timeout=None)
     hh_text = hh_page.text
     soup = BeautifulSoup(hh_page.text, 'html.parser')
-
-    hh_file = open(hh_filename, 'a', encoding='utf-8')
 
     line = 0
     companies = soup.find_all(class_='bloko-link bloko-link_secondary')
