f6998348c0e6b887a86796e4fe625736d94945fc
SiliconCamp <camp@silicon-camp.ru>
2018-12-12 15:42:02 +0300

    We now add the unique ID of CV before the each snapshot

--- hhcv_scalp.py
+++ hhcv_scalp.py
@@ -10,10 +10,15 @@ soup = BeautifulSoup(hh_file, 'html.parser')  # Просим BeautifulSoup "ра
 # страничку на элементы, создав объект soup
 
 cvs = soup.find_all(class_="resume-search-item__content-wrapper")
+cvids = soup.find_all(class_="bloko-toggle HH-Employer-VacancyResponse-Topic-ExperienceTrigger")
 
-for line in cvs:
-    cv_file.write(str(line))
-    cv_file.write("\n\n\n <br>------------------------------<br> \n\n\n")
+# data-hh-last-experience-id
+
+for line in range(1, len(cvs)):
+    cv_file.write("\n\n\n <br>--- CV#:" + str(cvids[line].get('data-hh-last-experience-id')) +
+                  " ---<br> \n\n\n")
+    cv_file.write(str(cvs[line]))
+
 
 hh_file.close()
 cv_file.close()
